6435ec7ba92cf1a3a21f078e3294b3c6162d577f
Dannte <danntegomez@gmail.com>
2025-03-28 18:08:22 -0500

    Signup configuration

--- src/signup.php
+++ src/signup.php
@@ -8,17 +8,39 @@
     $email  = $_POST['e_mail'];
     $passwd = $_POST['passw'];
 
-    $enc_pass = md5($passwd);
+    // Para encriptar la contraseña (2 opciones)
+    //$enc_pass = md5($passwd);
+    $enc_pass = sha1($passwd);
 
-    $sql = "INSERT INTO users (firstname, lastname, email, password)
+    //para que el correo email no se repita 
+    $sql_email_exist = 
+        "SELECT 
+            COUNT(email) as total 
+        FROM 
+            users 
+        WHERE 
+            email = '$email' 
+            LIMIT 1 ";
+    $res = pg_query($conn, $sql_email_exist);
+
+    if($res){
+        $row = pg_fetch_assoc($res);
+        if($row['total'] > 0){
+            echo "Email alredy exists";
+        }else {
+            $sql = "INSERT INTO users (firstname, lastname, email, password)
                 VALUES('$fname', '$lname', '$email', '$enc_pass')";
 
-    $res = pg_query($conn, $sql);
+            $res = pg_query($conn, $sql);
 
-    if ($res){
-        echo "User has been created succesfully";
-    }else {
-        echo "Error";
+            if ($res){
+                echo "User has been created succesfully";
+            }else {
+                echo "Error";
+            }
+        }
     }
+
+    
 
 ?>
